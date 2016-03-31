8f167f94d754720ef00ea8830866b05acbb98a34
Nikos Katsikanis <nKatsikanis@gmail.com>
2016-03-31 14:41:08 +0100

    add prod build with steal-tools

---
+++ build.js
@@ -0,0 +1,11 @@
+var stealTools = require("steal-tools");
+
+var promise = stealTools.build({
+  main: "router",
+  config: __dirname+"/package.json!npm"
+},{
+  // the following are the default values, so you don't need
+  // to write them.
+  minify: true,
+  debug: true
+});
