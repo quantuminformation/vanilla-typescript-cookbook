cb81c96ab072afad661d15cf661ad7b4fe5e6c45
Nikos Katsikanis <nKatsikanis@gmail.com>
2016-04-09 22:07:44 +0100

    new build

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 var stealTools = require("steal-tools");
 
 var promise = stealTools.build({
-  main: "router",
+  main: "app/router",
   config: __dirname + "/package.json!npm"
 }, {
   // the following are the default values, so you don't need
